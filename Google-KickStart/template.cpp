f1728f466181586f502aaa933b74aebfb7d3100b
Pratham Jaiswal <dev.pr47h4m@gmail.com>
2021-02-13 10:40:16 +0530

    template

---
+++ Google-KickStart/template.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+    // CODE BEGINS
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    int T;
+    cin >> T;
+    int C = 0;
+    while (T--)
+    {
+        C++;
+        cout << "Case #" << C << ": ";
+        solve();
+    }
+    return 0;
+}
