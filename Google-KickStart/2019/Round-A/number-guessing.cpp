809c8379fa9e46651dfdef04a486b08bd4cf02c7
Pratham Jaiswal <dev.pr47h4m@gmail.com>
2021-02-13 10:47:09 +0530

    transformed

--- Google-KickStart/2019/Round-A/number-guessing.cpp
+++ Google-KickStart/2019/Round-A/number-guessing.cpp
@@ -2,31 +2,37 @@
 
 using namespace std;
 
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+    int n;
+    cin >> n;
+    string res;
+    while (n-- && res != "CORRECT" && res != "WRONG_ANSWER")
+    {
+        cout << (a + b + 1) / 2 << endl;
+        cin >> res;
+        if (res == "TOO_SMALL")
+        {
+            a = (a + b + 1) / 2 + 1;
+        }
+        else if (res == "TOO_BIG")
+        {
+            b = (a + b + 1) / 2 - 1;
+        }
+    }
+}
+
 int main()
 {
-    int t; // for t testcases
-    cin >> t;
-    while (t--)
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T; // for t testcases
+    cin >> T;
+    while (T--)
     {
-        int a, b;
-        cin >> a >> b;
-        int n;
-        cin >> n;
-        string res;
-        while (n-- && res != "CORRECT" && res != "WRONG_ANSWER")
-        {
-            cout << (a + b + 1) / 2 << endl;
-            fflush(stdout);
-            cin >> res;
-            if (res == "TOO_SMALL")
-            {
-                a = (a + b + 1) / 2 + 1;
-            }
-            else if (res == "TOO_BIG")
-            {
-                b = (a + b + 1) / 2 - 1;
-            }
-        }
+        solve();
     }
     return 0;
 }
