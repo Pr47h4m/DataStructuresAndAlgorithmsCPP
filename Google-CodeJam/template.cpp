5cbeb151e1927ea67570a97c4a8fa1ab0d178a30
Pratham Jaiswal <dev.pr47h4m@gmail.com>
2021-02-18 10:54:46 +0530

    template for code jam

---
+++ Google-CodeJam/template.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+void solve() {
+    // CODE BEGINS
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    int T;
+    cin >> T;
+    int C = 0;
+    while (T--)
+    {
+        C++;
+        cout << "Case #" << C << ": ";
+        solve();
+    }
+    return 0;
+}
